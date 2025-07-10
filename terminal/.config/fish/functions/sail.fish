6b31a93ad62b9e1f3882c96073c2da7b5e4b9ab0
Andrej Cremoznik <andrej@cremoznik.si>
2025-07-10 10:28:14 +0200

    Add Docker context check for sail

--- terminal/.config/fish/functions/sail.fish
+++ terminal/.config/fish/functions/sail.fish
@@ -1,3 +1,9 @@
 function sail
+  set context "$(docker context inspect --format '{{.Name}}')"
+  if test $context != default
+    if not prompt_confirm "Docker context is $context. Continue?"
+      return 0
+    end
+  end
   ./vendor/bin/sail $argv
 end
